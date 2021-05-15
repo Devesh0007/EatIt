71fe3b948d993aee98d3e0a755c16cd7f54a42e0
Devesh Sahu <sahudevesh07@gmail.com>
2021-05-15 21:28:04 +0530

    Added all files

---
+++ app/src/main/java/com/example/android/eatit/Common/Config.java
@@ -0,0 +1,6 @@
+package com.example.android.eatit.Common;
+
+public class Config {
+
+    public static final String PAYPAL_CLIENT_ID="AS0qlDSmHYLvPf8qvOctwdMh5WvWingJtFPsMrI1WrY30xUrfxIkHy2VYkNikmFPDJOWY_A7g81otKcw";
+}
